ac459da7c27828c1c7bea6f5a67c332b1c9017d0
Grzegorz <grzegorz@Grzegorzs-MacBook-Air.local>
2017-05-13 21:17:26 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,141 @@
+var BouncingBall = function (THREE, TWEEN, GUI, Stats) {
+    var renderer;
+    var scene;
+    var camera;
+    var control;
+    var stats;
+    var ball;
+
+    function init() {
+
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setClearColor(0x000000, 1.0);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.shadowMapEnabled = true;
+        new THREE.OrbitControls(camera, renderer.domElement);
+
+        var planeGeometry = new THREE.PlaneGeometry(20, 20);
+        var planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
+        var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.receiveShadow = true;
+
+        plane.rotation.x = -0.5 * Math.PI;
+        plane.position.x = 0;
+        plane.position.y = -2;
+        plane.position.z = 0;
+
+        scene.add(plane);
+
+        var ballGeometry = new THREE.SphereGeometry(4, 100, 100);
+        var cubeMaterial = new THREE.MeshPhongMaterial({map: THREE.ImageUtils.loadTexture('img/dino-texture.jpg')});
+        ball = new THREE.Mesh(ballGeometry, cubeMaterial);
+        ball.name = 'cube';
+        ball.castShadow = true;
+        ball.position.y = 10;
+
+        scene.add(ball);
+
+        camera.position.x = 15;
+        camera.position.y = 16;
+        camera.position.z = 13;
+        camera.lookAt(scene.position);
+
+        var spotLight = new THREE.SpotLight(0xffffff);
+        spotLight.position.set(10, 20, 20);
+        spotLight.shadowCameraNear = 20;
+        spotLight.shadowCameraFar = 50;
+        spotLight.castShadow = true;
+
+        scene.add(spotLight);
+
+        var light = new THREE.PointLight(0x00ffff, 1, 100);
+        light.position.set(20, 0, 0);
+        scene.add(light);
+
+        control = new function () {
+            this.rotationSpeed = 0.005;
+            this.opacity = 0.6;
+            this.color = cubeMaterial.color.getHex();
+        };
+
+        addControlGui(control);
+        addStatsObject();
+
+        document.body.appendChild(renderer.domElement);
+
+        setupTweens();
+        render();
+    }
+
+    function setupTweens() {
+        TWEEN.removeAll();
+
+        var position = {y: 10};
+
+        var targetDown = {y: 2};
+        var tweenDown = new TWEEN.Tween(position)
+            .to(targetDown, 1000)
+            .onUpdate(function () {
+                ball.position.y = position.y;
+            });
+        tweenDown.easing(TWEEN.Easing.Quadratic.In);
+
+        var targetUp = {y: 10};
+        var tweenUp = new TWEEN.Tween(position)
+            .to(targetUp, 1000)
+            .onUpdate(function () {
+                ball.position.y = position.y;
+            });
+        tweenUp.easing(TWEEN.Easing.Quadratic.Out);
+
+        tweenDown.chain(tweenUp);
+        tweenUp.chain(tweenDown);
+
+        tweenDown.start();
+    }
+
+    function addControlGui(controlObject) {
+        var gui = new GUI();
+        gui.add(controlObject, 'rotationSpeed', -0.01, 0.01);
+        gui.addColor(controlObject, 'color');
+    }
+
+    function addStatsObject() {
+        stats = new Stats();
+        stats.setMode(0);
+
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+
+        document.body.appendChild(stats.domElement);
+    }
+
+    function render() {
+        ball.rotation.y += control.rotationSpeed * 10;
+
+        scene.getObjectByName('cube').material.color = new THREE.Color(control.color);
+
+        stats.update();
+
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(render);
+
+        TWEEN.update();
+    }
+
+    function handleResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    return {
+        'init': init,
+        'handleResize': handleResize
+    }
+};
